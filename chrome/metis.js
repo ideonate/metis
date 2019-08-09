c89ee097bab21f89917ec441d820f31596195901
Dan Lester <dan@danlester.com>
2019-08-09 10:50:38 +0100

    Popup comms improved

--- chrome/metis.js
+++ chrome/metis.js
@@ -1,24 +1,42 @@
 "use strict";
 
-var msgs = [];
+let tabmap = new Map();
 
-var port = null;
-var getKeys = function(obj){
-	var keys = [];
-	for(var key in obj){
-		keys.push(key);
-	}
-	return keys;
-}
+let servermap = new Map();
+
+let uidcounter = 1;
+
+let globallogs = [];
+
+let port = null;
+
 function appendMessage(text) {
-	msgs.push(text);
+	globallogs.push(text);
 }
 
-function sendNativeMessage(msg) {
+function sendNativeMessage(serverobj, msg) {
+	msg.uid = serverobj.uid;
 	port.postMessage(msg);
 	appendMessage("Sent message: <b>" + JSON.stringify(msg) + "</b>");
+
+	if (msg.cmd == 'start') {
+		serverobj.status = 2;
+		serverobj.locallogs.push('Sent native start command');
+		sendForPopup(serverobj);
+	}
 }
+
+function sendForPopup(serverobj) {
+	chrome.runtime.sendMessage({uid: serverobj.uid,
+		port: serverobj.port,
+		status: serverobj.status,
+		locallogs: serverobj.locallogs,
+		globallogs: globallogs
+	});
+}
+
 function onNativeMessage(msg) {
+	//let uid = msg.uid;
 	appendMessage("Received message: <b>" + JSON.stringify(msg) + "</b>");
 }
 function onDisconnected() {
@@ -54,12 +72,51 @@ async function start() {
 			console.log(sender.tab ?
 				"from a content script:" + sender.tab.url :
 				"from the extension");
-				if (request.msg) {
-					sendNativeMessage(request.msg);
+			if (!sender.tab) {
+
+				let serverobj = {};
+				let uid = request.uid;
+
+				if (request.tabid) {
+					if (tabmap.has(request.tabid)) {
+						uid = tabmap.get(request.tabid);
+					}
 				}
-				sendResponse({port: port != null,
-					msgs: msgs
+
+				let retval = {};
+
+				if (!uid) {
+					uid = uidcounter++;
+					serverobj.uid = uid;
+					serverobj.port = 0;
+					serverobj.locallogs = ['Starting with uid: '+uid];
+					serverobj.status = 1; // Stopped
+					servermap.set(uid, serverobj);
+
+					if (request.tabid) {
+						tabmap.set(request.tabid, uid);
+					}
+				}
+				else {
+					serverobj = servermap.get(uid);
+				}
+
+				sendResponse({uid: uid,
+					port: serverobj.port,
+					status: serverobj.status,
+					locallogs: serverobj.locallogs,
+					globallogs: globallogs
 				});
+
+				// Start or stop server?
+				if (request.cmd) {
+					serverobj.status = (request.status + 1) % 5;
+					if (request.status == 1 || request.status == 3) {
+						sendNativeMessage(serverobj, {cmd:'start'});
+					}
+				}
+			}
+
 		});
 
 	chrome.browserAction.setPopup({'popup':'main.html'}, popupCallback);
--- chrome/popup.js
+++ chrome/popup.js
@@ -1,54 +1,101 @@
 "use strict";
 
-var isport = false;
+let port = 0;
 
-function appendMessage(text) {
-    document.getElementById('response').innerHTML += "<p>" + text + "</p>";
-}
+let status = 0; /* 0 = loading, 1 = stopped, 2 = launching server, 3 = running, 4 = stopping */
+
+let statustext = ['Loading', 'Dead', 'Launching', 'Live', 'Stopping'];
+
+let uid = '';
+
 
 function updateUiState() {
-    if (isport) {
+    if (status == 0) {
+        document.getElementById('connect-button').disabled = true;
+        document.getElementById('connect-button').text = 'Loading...';
+        document.getElementById('connect-button').style.display = 'none';
+    }
+    else if (status == 1) {
+        document.getElementById('connect-button').disabled = false;
+        document.getElementById('connect-button').text = 'Run';
         document.getElementById('connect-button').style.display = 'block';
-        document.getElementById('input-text').style.display = 'block';
-        document.getElementById('send-message-button').style.display = 'block';
-    } else {
-        document.getElementById('connect-button').style.display = 'block';
-        document.getElementById('input-text').style.display = 'none';
-        document.getElementById('send-message-button').style.display = 'none';
+    } else if (status == 2) {
+        document.getElementById('connect-button').text = 'Launching...';
+        document.getElementById('connect-button').disabled = true;
+    } else if (status == 3) {
+        document.getElementById('connect-button').disabled = false;
+        document.getElementById('connect-button').text = 'Stop';
+    }
+    else if (status == 4) {
+        document.getElementById('connect-button').disabled = true;
+        document.getElementById('connect-button').text = 'Stopping...';
     }
 }
 
 function connect() {
-    sendObject({});
+    sendObject({cmd: 'start'});
 }
 
-function sendNativeMessage() {
-    let message = {"text": document.getElementById('input-text').value};
-    sendObject({msg: message});
-}
+function messageResponder(response) {
+    console.log(response);
+    uid = response.uid;
+    port = response.port;
+    status = response.status;
+
+    document.getElementById('status').innerHTML = statustext[status];
+
+    document.getElementById('info').innerHTML = '<p>UID: '+uid+'</p><p>Port: '+port+'</p><p>Status: '+status+'</p>';
+
+    let locallogs = document.getElementById('locallogs');
+    locallogs.innerHTML = '';
+    for (var i=0 ; i<response.locallogs.length ; ++i) {
+        locallogs.innerHTML += "<p>" + response.locallogs[i] + "</p>";
+    }
+
+    let globallogs = document.getElementById('globallogs');
+    globallogs.innerHTML = '';
+    for (var i=0 ; i<response.globallogs.length ; ++i) {
+        globallogs.innerHTML += "<p>" + response.globallogs[i] + "</p>";
+    }
+
+    updateUiState();
+};
 
 function sendObject(o) {
     console.log('Sending object');
-    console.log(o);
-    chrome.runtime.sendMessage(o, function(response) {
-        console.log(response);
-        document.getElementById('response').innerHTML = '';
-        for (var i=0 ; i<response.msgs.length ; ++i) {
-            appendMessage(response.msgs[i]);
-        }
-        isport = response.port;
 
-        updateUiState();
-    });
+    o.uid = uid;
+    o.status = status;
+    o.port = port;
+
+    console.log(o);
+    chrome.runtime.sendMessage(o, messageResponder);
 }
 
 document.addEventListener('DOMContentLoaded', function () {
     console.log("DOM loaded popup");
+
     document.getElementById('connect-button').addEventListener(
         'click', connect);
-    document.getElementById('send-message-button').addEventListener(
-        'click', sendNativeMessage);
-    updateUiState();
+
+    chrome.runtime.onMessage.addListener(
+        function(request, sender, sendResponse) {
+            messageResponder(request);
+        }
+    );
+
+    chrome.tabs.query({active: true}, function(tabs){
+
+        if (tabs.length != 1) {
+            alert("Multiple active tabs or none");
+            console.log(tabs);
+        }
+        else {
+            sendObject({tabid: tabs[0].id});
+
+            updateUiState();
+        }
+    });
 
 });
 
