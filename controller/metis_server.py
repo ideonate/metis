049e4731aa484b7138511e2d6a044dee71259e1f
Dan Lester <dan@danlester.com>
2019-08-12 17:45:23 +0100

    Two-way comms working in threads

--- controller/metis_server.py
+++ controller/metis_server.py
@@ -8,13 +8,13 @@ import sys
 import threading
 import queue
 import json
-#try:
-#    import tkinter
-#    from tkinter import messagebox
-#except ImportError:
-#    tkinter = None
+try:
+    import tkinter
+    from tkinter import messagebox
+except ImportError:
+    tkinter = None
 
-tkinter = None
+# tkinter = None
 
 import subprocess
 
@@ -38,7 +38,7 @@ def send_message(message):
     sys.stdout.buffer.flush()
 
 
-def jpipe_run(req, fromjqueue, q):
+def jpipe_run(req, fromjqueue, tojqueue, q): # req['cmd']=='start'
     # 0 = loading, 1 = stopped, 2 = launching server, 3 = running, 4 = stopping
     uid = req['uid']
     jpipe = None
@@ -49,80 +49,73 @@ def jpipe_run(req, fromjqueue, q):
     try:
         virtualenv = req['virtualenv']
         homedir = req['homedir']
-        jpipe = subprocess.Popen(['./run_jupyter.sh', virtualenv, homedir], stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=1, text=True, shell=False)
+        jpipe = subprocess.Popen(['./run_jupyter.sh', virtualenv, homedir], stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                 stdin=subprocess.PIPE, bufsize=1, text=True, shell=False)
 
     except Exception as e:
         if q:
             q.put(str(e))
         fromjqueue.put({'uid': uid, 'status': 1, 'msg': 'Failed jpipe: '+str(e)})
-        sys.exit(0)
+        return
 
     fromjqueue.put({'uid': uid, 'msg': 'Started jpipe'})
 
     if q:
         q.put('Started jpipe in thread: pyversion '+sys.version)
 
-    while jpipe.poll() is None:
+    def stderr_stream(jpipe, fromjqueue, q):
+        while jpipe.poll() is None:
+            l = jpipe.stderr.readline()
+            if l != '':
+                fromjqueue.put(json.dumps({'uid':uid, 'errmsg': l}))
 
-        try:
-            # outs, errs = jpipe.communicate(None, timeout=1)
-            outs = jpipe.stdout.readline()
+    def stdout_stream(jpipe, fromjqueue, q):
+        while jpipe.poll() is None:
+            l = jpipe.stdout.readline()
+            if l != '':
+                try:
+                    d = json.loads(l)
 
+                    if 'server_info' in d:
+                        fromjqueue.put({'uid': uid, 'status': 3, 'server_info': d['server_info']})
+                except json.JSONDecodeError as jde:
+                    if q:
+                        q.put(str(jde))
+                    fromjqueue.put({'uid': uid, 'status': 4, 'msg': 'JSON decode error: '+l})
+
+    def inqueue_monitor(jpipe, tojqueue, q):
+        while jpipe.poll() is None:
+            req = tojqueue.get()
             if q:
-                q.put('outs: ' + outs) #.decode('utf-8'))
-                #q.put('errs: ' + errs) #.decode('utf-8'))
+                q.put('STDIN SEND: '+json.dumps(req))
+            if req['cmd'] == 'stop':
+                jpipe.stdin.write(json.dumps({'cmd': 'stop'})+"\n")
+                jpipe.stdin.flush()
 
-            try:
-                d = json.loads(outs)
+    errthread = threading.Thread(target=stderr_stream, args=(jpipe, fromjqueue, q,))
+    errthread.daemon = True
+    errthread.start()
 
-                if 'server_info' in d:
-                    fromjqueue.put({'uid': uid, 'status': 3, 'server_info': d['server_info']})
+    outthread = threading.Thread(target=stdout_stream, args=(jpipe, fromjqueue, q,))
+    outthread.daemon = True
+    outthread.start()
 
-            except json.JSONDecodeError as jde:
-                if q:
-                    q.put(str(jde))
-                fromjqueue.put({'uid': uid, 'status': 4, 'msg': 'JSON decode error: '+outs})
+    inthread = threading.Thread(target=inqueue_monitor, args=(jpipe, tojqueue, q,))
+    inthread.daemon = True
+    inthread.start()
 
-        except Exception as e:
-            if q:
-                q.put(str(e))
-            fromjqueue.put({'uid': uid, 'status': 4, 'msg': str(e)})
+    jpipe.wait()
 
-    jpipe = None
     if q:
         q.put('Finished jpipe')
     fromjqueue.put({'uid': uid, 'status': 1, 'msg': 'STOPPED jpipe'})
 
-def jport_stop(req, fromjqueue, q):
-    try:
-        uid = req['uid']
-        port = req['server_info']['port']
-        virtualenv = req['virtualenv']
-        homedir = req['homedir']
 
-        if q:
-            q.put('Stopping jport for uid {} port {}'.format(uid, port))
-
-        fromjqueue.put({'uid': uid, 'msg': 'Stopping port'})
-        p = subprocess.run(['./stop_jupyter.sh', virtualenv, homedir, str(port)], text=True, shell=False, capture_output=True)
-        fromjqueue.put({'uid': uid, 'msg': p.stderr + p.stdout})
-
-    except Exception as e:
-        if q:
-            q.put(str(e))
-        fromjqueue.put({'uid': uid, 'status': 1, 'msg': 'Failed jport: '+str(e)})
-        sys.exit(0)
-
-
-def start_jpipe(req, fromjqueue, q):
-    thread = threading.Thread(target=jpipe_run, args=(req, fromjqueue, q,))
+def start_jpipe(req, fromjqueue, tojqueue, q):
+    thread = threading.Thread(target=jpipe_run, args=(req, fromjqueue, tojqueue, q,))
     thread.daemon = True
     thread.start()
 
-def stop_jport(req, fromjqueue, q):
-    thread = threading.Thread(target=jport_stop, args=(req, fromjqueue, q,))
-    thread.daemon = True
-    thread.start()
 
 # Thread that reads messages from the webapp.
 def read_thread_func(q):
@@ -151,17 +144,14 @@ def read_thread_func(q):
 
         if uid in uidtojqueues: # thread-safe because we only read input in this thread
 
-            if req['cmd'] == 'stop': # stop is initiated through another process
-                stop_jport(req, fromjqueue, q)
-
-            else: # Don't currently have any way to catch these messages anyway - only start!
-                tojqueue = uidtojqueues[uid]
-                tojqueue.put(req)
+           tojqueue = uidtojqueues[uid]
+           tojqueue.put(req)
 
         else:
-            uidtojqueues[uid] = queue.Queue()
+            tojqueue = queue.Queue()
+            uidtojqueues[uid] = tojqueue
             if req['cmd'] == 'start':
-                start_jpipe(req, fromjqueue, q)
+                start_jpipe(req, fromjqueue, tojqueue, q)
 
             if q:
                 q.put('Starting pipe')
--- controller/run_jupyter_instance.py
+++ controller/run_jupyter_instance.py
@@ -1,6 +1,28 @@
 from notebook.notebookapp import NotebookApp
 import json
 import sys
+import threading
+
+stdoutlock = threading.Lock()
+
+
+def read_stdin(stdoutlock, app):
+    while 1:
+        l = sys.stdin.readline()
+        try:
+            d = json.loads(l)
+            with stdoutlock:
+                print(json.dumps({'read_stdin_received':l}))
+                sys.stdout.flush()
+            if d['cmd'] == 'stop':
+                app.stop()
+        except json.JSONDecodeError as jde:
+            with stdoutlock:
+                print(json.dumps({'decodeerr':str(jde)}))
+                sys.stdout.flush()
+
+
+# Init the Jupyter app
 
 kwargs = {}
 argv = ['--no-browser']
@@ -8,11 +30,17 @@ argv = ['--no-browser']
 app = NotebookApp.instance(**kwargs)
 app.initialize(argv)
 
-print(json.dumps({'server_info': app.server_info()}))
-sys.stdout.flush()
+with stdoutlock:
+    print(json.dumps({'server_info': app.server_info()}))
+    sys.stdout.flush()
+
+inthread = threading.Thread(target=read_stdin, args=(stdoutlock, app,))
+inthread.daemon = True
+inthread.start()
 
 app.start()
 
-print('{"cmd":"stopped"}')
-sys.stdout.flush()
+with stdoutlock:
+    print(json.dumps({'cmd':'stopped'}))
+    sys.stdout.flush()
 
